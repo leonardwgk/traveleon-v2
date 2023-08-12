004524aba7adb02bd4c3f7bea58c92771e020593
leonardwgk <leonardwangka@gmail.com>
2023-08-12 14:01:03 +0700

    update middleware group

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -18,6 +18,6 @@ class AdminMiddleware
         if (auth()->user()->role === 'admin') {
             return $next($request);
         }
-        return route('login');
+        return redirect('/login');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -27,31 +27,35 @@ Route::get('/register', [AuthController::class, 'register']);
 Route::post('/register/create', [AuthController::class, 'store']);
 
 //user
-Route::get('/user', [TiketController::class, 'index'])->middleware('auth');
-Route::post('/user/pesan', [TiketController::class, 'store'])->middleware('auth');
-Route::get('/user/account', [TiketController::class, 'daftar'])->middleware('auth');
-Route::get('/user/wisata', [WisataController::class, 'indexuser']);
-//detail tiket
-Route::get('/detail/{id}', [TiketController::class, 'detail'])->middleware('auth');
+Route::middleware(['auth', 'user'])->group(function () {
+    Route::get('/user', [TiketController::class, 'index']);
+    Route::post('/user/pesan', [TiketController::class, 'store']);
+    Route::get('/user/account', [TiketController::class, 'daftar']);
+    Route::get('/user/wisata', [WisataController::class, 'indexuser']);
+    //detail tiket
+    Route::get('/detail/{id}', [TiketController::class, 'detail']);
+});
 
 
 //admin
-Route::get('/admin', [AdminController::class, 'index'])->middleware('auth');
-Route::get('/admin/manage', [AdminController::class, 'manage'])->middleware('auth');
-//edit akun
-Route::get('/admin/edit/{id}', [AdminController::class, 'edit'])->middleware('auth');
-Route::put('/admin/update/{id}', [AdminController::class, 'update'])->middleware('auth');
-Route::delete('/admin/manage/delete/{id}', [AdminController::class, 'delete'])->middleware('auth');
-//approve tiket
-Route::put('/admin/approve/{id}', [AdminController::class, 'approve'])->middleware('auth');
-Route::delete('/admin/delete/{id}', [AdminController::class, 'destroy'])->middleware('auth');
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index']);
+    Route::get('/admin/manage', [AdminController::class, 'manage']);
+    //edit akun
+    Route::get('/admin/edit/{id}', [AdminController::class, 'edit']);
+    Route::put('/admin/update/{id}', [AdminController::class, 'update']);
+    Route::delete('/admin/manage/delete/{id}', [AdminController::class, 'delete']);
+    //approve tiket
+    Route::put('/admin/approve/{id}', [AdminController::class, 'approve']);
+    Route::delete('/admin/delete/{id}', [AdminController::class, 'destroy']);
 
-//wisata
-Route::get('/admin/wisata', [WisataController::class, 'index'])->middleware('auth');
-Route::post('/admin/wisata/store', [WisataController::class, 'store'])->middleware('auth');
-Route::get('admin/wisata/edit/{id}', [WisataController::class, 'edit'])->middleware('auth');
-Route::put('admin/wisata/update/{id}', [WisataController::class, 'update'])->middleware('auth');
-Route::delete('admin/wisata/delete/{id}', [WisataController::class, 'delete'])->middleware('auth');
+    //wisata
+    Route::get('/admin/wisata', [WisataController::class, 'index']);
+    Route::post('/admin/wisata/store', [WisataController::class, 'store']);
+    Route::get('/admin/wisata/edit/{id}', [WisataController::class, 'edit']);
+    Route::put('/admin/wisata/update/{id}', [WisataController::class, 'update']);
+    Route::delete('/admin/wisata/delete/{id}', [WisataController::class, 'delete']);
+});
 
 
 Route::get('/logout', [AuthController::class, 'logout']);
